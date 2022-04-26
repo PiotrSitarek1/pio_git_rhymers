4cbe323a3a3c11665dc9af1a05528452fa1f4ad5
LAPTOP-F1REOL2V\LEGION <235969@edu.p.lodz.pl>
2022-04-26 12:21:25 +0200

    11. Usunięcie nieuzywanych settery

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -19,10 +19,6 @@ public class DefaultCountingOutRhymer {
         return MAX_SIZE;
     }
 
-    public void setCapacity(int a) {
-       CAPACITY = a;
-    }
-
     public int getTotal() {
         return total;
     }
